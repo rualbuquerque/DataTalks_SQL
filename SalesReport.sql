b22356b6a7ec423c4e469ad975e80b86de9cb807
Ruby <rubiaalbuquerque@outlook.com>
2020-07-21 11:43:41 -0400

    Update SalesReport.sql

--- SalesReport.sql
+++ SalesReport.sql
@@ -15,7 +15,9 @@ FROM [AdventureWorks2012].[Sales].[SalesOrderDetail]
 GROUP BY ProductID
 ORDER BY TotalSales DESC
 
-
+/*You need to use the Order By otherwise your TOP 10 will be random. If you don't put 'DESC' the results will be the smalest sales,
+because the order by will show de results ASC by default.
+*/
 --------------------------------------------------------------------------------
 -------------------------- 10 most popular products 
 --------------------------------------------------------------------------------
@@ -42,6 +44,14 @@ INNER JOIN
 WHERE YEAR(SH.OrderDate) = 2005
 GROUP BY DATENAME(MONTH,SH.OrderDate), MONTH(SH.OrderDate)
 ORDER BY MONTH(SH.OrderDate)
+				
+
+/*
+
+To have the results we want here we combine the GROUP BY clause and an aggregate function. You also need to mention in the group by the column that is
+being used in the order by clause, otherwise you code will crash. 
+
+*/
 
 --------------------------------------------------------------------------------
 ------------------------------- Sales by Country
